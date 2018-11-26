6a1f14fed91f6eb6737a5ab16bb716e004fae969
王博 <bo.wang1016@outlook.com>
2018-11-26 18:21:34 +0800

    message:0.0.7

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # GridManager-ccms-skin
 
 GridManager适配组件库样式
+
+
+## 修改原始配置项
+
+- dataKey 设置为'list'
+- currentPageKey 设置为'pageNum'
+- pageSizeKey 设置为'pageSize'
+- supportAutoOrder 设置为false
+- supportCheckbox 设置为false
+- supportAjaxPage 设置为true
+- supportAdjust 设置为true
+- disableLine 设置为true
+- supportMenu 设置为false
+- showFooterPageInfo 设置为true
+- showFooterGoTo 设置为false
+- showFooterCheckedInfo 设置为false
+- loadingTemplate 设置默认loading模版
+- refresh-action 设置刷新按钮模版
+- textConfig: 翻页样式：设置了使用图标样式代替文本样式
+    - next-page
+    - previous-page
+    - last-page
+    - first-page
+    - page-info 自定义了页信息
+
+
--- src/js/index.js
+++ src/js/index.js
@@ -8,49 +8,56 @@ import './gridmanager-reset.less';
     const defaultOption = {
         width: '100%',
         height: '100%',
-        currentPageKey: 'pageNum', // 默认请求参数当页字段
+        currentPageKey: 'pageNum',// 默认请求参数当页字段
         pageSizeKey: 'pageSize',  // 默认请求参数当页显示数据条数
-        supportAutoOrder: false, // 默认不支持自动产生序号
-        supportCheckbox: false,  // 默认不支持使用复选框选中
-        supportAjaxPage: true,   // 默认支持分页
-        supportAdjust: true,    // 默认不支持列宽度调整
-        disableLine: true,       // 默认不支持列分割线显示
-        supportMenu: false,      // 默认不支持右键
-        dataKey: 'list',       // 数据使用后端返回的list数组
-        showFooterPageInfo: true,
-        showFooterGoTo: false,
-        showFooterCheckedInfo: false,
+        supportAutoOrder: false,  // 默认不支持自动产生序号
+        supportCheckbox: false,   // 默认不支持使用复选框选中
+        supportAjaxPage: true,    // 默认支持分页
+        supportAdjust: true,      // 默认不支持列宽度调整
+        disableLine: true,        // 默认不支持列分割线显示
+        supportMenu: false,       // 默认不支持右键
+        dataKey: 'list',          // 数据使用后端返回的list数组
+        showFooterPageInfo: true, // 默认显示页数据信息
+        showFooterGoTo: false,    // 默认不显示自带的跳转框
+        showFooterCheckedInfo: false,  // 默认不显示选中描述信息
+        skinClassName: 'ccms-skin', // 页样式名称
+        loadingTemplate: `<section class="gm-ccms-loading">
+                            <div class="loader">
+                                <svg class="circular" viewBox="25 25 50 50">
+                                    <circle class="path" cx="50" cy="50" r="20" fill="none"></circle>
+                                </svg>
+                            </div>
+                        </section>`,
         textConfig: {
             'next-page': {
                 'zh-cn': '<i class="iconfont icon-nextpage"></i>',
+                'zh-tw': '下一頁',
                 'en-us': 'next'
             },
             'previous-page': {
                 'zh-cn': '<i class="iconfont icon-nextpage"></i>',
+                'zh-tw': '上一頁',
                 'en-us': 'previous'
             },
             'last-page': {
                 'zh-cn': '<i class="iconfont icon-lastpage"></i>',
+                'zh-tw': '尾頁',
                 'en-us': 'last'
             },
             'first-page': {
                 'zh-cn': '<i class="iconfont icon-lastpage"></i>',
+                'zh-tw': '首頁',
                 'en-us': 'first'
+            },
+            'refresh-action': {
+                'zh-cn': '<i class="iconfont icon-refresh"></i><span class="refresh-label">刷新</span>',
+                'zh-tw': '<i class="iconfont icon-refresh"></i>',
+                'en-us': '<i class="iconfont icon-refresh"></i>'
             },
             'page-info': {
                 'zh-cn': '<span class="page-info-totals">共{2}条</span><span class="page-size-info"><input type="text" class="gp-input" value="{3}"/>/ 共{4}页</span>',
-                'zh-tw': '尾頁',
-                'en-us': 'pageInfo'
-            },
-            'goto-first-text': {
-                'zh-cn': '跳转至',
-                'zh-tw': '跳轉至',
-                'en-us': 'goto'
-            },
-            'goto-last-text': {
-                'zh-cn': '页',
-                'zh-tw': '頁',
-                'en-us': 'page'
+                'zh-tw': '此頁顯示 {0}-{1} 共{2}條',
+                'en-us': 'this page show {0}-{1} count {2}'
             }
         }
     };
